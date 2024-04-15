1c66914da46e65681323e5bc89824a9860983f51
b-walton <benjamin.t.walton@vanderbilt.edu>
2024-04-15 08:21:20 -0500

    Finish form validation and macro retrieval

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { ChakraProvider, theme } from "@chakra-ui/react"
 import { GetUserInformation } from "./GetUserInformation"
 export const App = () => {
-
+  
   return (
     <ChakraProvider theme={theme}>
       <GetUserInformation></GetUserInformation>
